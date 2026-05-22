ee217db4c6c19f5b2f2749dd0acfa2cc5ba24956
Eirom <elizabethmatos983@gmail.com>
2026-05-21 21:21:45 -0400

    release: bump version to v1.1.2 and switch openSUSE container to tumbleweed

--- src/pyrolist/main.py
+++ src/pyrolist/main.py
@@ -72,7 +72,7 @@ def main() -> None:
             
     app = QApplication(sys.argv)
     app.setApplicationName("Pyrolist")
-    app.setApplicationVersion("1.1.1")
+    app.setApplicationVersion("1.1.2")
     app.setOrganizationName("pyrolist")
 
     setup_vlc_env()
